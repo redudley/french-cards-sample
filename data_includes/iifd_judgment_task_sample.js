802f18b8887a745b9c8b9544c30a0633cc86ebdc
Rachel Dudley <rachel.elaine.dudley@gmail.com>
2019-04-08 19:04:44 +0200

    Update iifd_judgment_task_sample.js

--- data_includes/iifd_judgment_task_sample.js
+++ data_includes/iifd_judgment_task_sample.js
@@ -2,12 +2,15 @@
 // See the documentation for more information:
 // 		https://lab.florianschwarz.net/PennController/wiki/documentation/
 
-PennController.Sequence( "instructions", randomize("itemNameHERE");
+PennController.Sequence( "preload", "instructions", randomize("itemNameHERE");
           //ACTION: check order of these items
 PennController.ResetPrefix(null);
 PennController.AddHost("http://web-risc.ens.fr/~rdudley/sample/");
           //ACTION: update name of server if necessary
           //ACTION: make sure files can be acquired in zipped version
+
+PennController.CheckPreloaded( startsWith("item") )
+    .label( "preload" );
 
 PennController( "instructions" ,
   newButton("start", "Start the pilot")
