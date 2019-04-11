f4ae1519b770c19a52ea852b99d4d5d3c9b5675a
Rachel Dudley <rachel.elaine.dudley@gmail.com>
2019-04-11 17:17:51 +0200

    trying to prevent not hearing domain

--- data_includes/iifd_judgment_task_sample.js
+++ data_includes/iifd_judgment_task_sample.js
@@ -44,6 +44,7 @@ PennController.Template(
           .settings.add(   750,   0, getImage("domainImage4") )
           .settings.add(   1000,  0, getImage("domainImage5") )
           .print()
+          .wait()
         ,
         newButton("ready", "Ready for the next item")
               //ACTION: change text of this button to french
