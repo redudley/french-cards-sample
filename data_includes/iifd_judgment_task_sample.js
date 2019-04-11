e97bcd35233e7613c311b4006edfe88389a51b52
Rachel Dudley <rachel.elaine.dudley@gmail.com>
2019-04-11 17:52:19 +0200

    typo

--- data_includes/iifd_judgment_task_sample.js
+++ data_includes/iifd_judgment_task_sample.js
@@ -15,27 +15,27 @@ PennController.AddHost("http://web-risc.ens.fr/~rdudley/sample/");
 PennController.Template(
     row => PennController("domain_trial",
         newImage("domainImage1", row.domainImage1)
-          .settings,.size(144,180)
+          .settings.size(144,180)
         //ACTION: figure out necessary picture size and reset
         ,
         newImage("domainImage2", row.domainImage2)
-          .settings,.size(144,180)
+          .settings.size(144,180)
         //ACTION: figure out necessary picture size and reset
         ,
         newImage("domainImage3", row.domainImage3)
-          .settings,.size(144,180)
+          .settings.size(144,180)
         //ACTION: figure out necessary picture size and reset
         ,
         newImage("domainImage4", row.domainImage4)
-          .settings,.size(144,180)
+          .settings.size(144,180)
         //ACTION: figure out necessary picture size and reset
         ,
         newImage("domainImage5", row.domainImage5)
-          .settings,.size(144,180)
+          .settings.size(144,180)
         //ACTION: figure out necessary picture size and reset
         ,
         newImage("domainImage6", row.domainImage5)
-          .settings,.size(144,180)
+          .settings.size(144,180)
         //ACTION: figure out necessary picture size and reset
         ,
         newCanvas("domain", 864, 180) //images need to be shuffled, or no?
@@ -69,23 +69,23 @@ PennController.Template(
           .settings.size(200,200)
         ,
         newImage("judgmentImage1", row.judgmentImage1)
-          .settings,.size(144,180)
+          .settings.size(144,180)
         //ACTION: figure out necessary picture size and reset
         ,
         newImage("judgmentImage2", row.judgmentImage2)
-          .settings,.size(144,180)
+          .settings.size(144,180)
         //ACTION: figure out necessary picture size and reset
         ,
         newImage("judgmentImage3", row.judgmentImage3)
-          .settings,.size(144,180)
+          .settings.size(144,180)
         //ACTION: figure out necessary picture size and reset
         ,
         newImage("judgmentImage4", row.judgmentImage4)
-          .settings,.size(144,180)
+          .settings.size(144,180)
         //ACTION: figure out necessary picture size and reset
         ,
         newImage("judgmentImage5", row.judgmentImage5)
-          .settings,.size(144,180)
+          .settings.size(144,180)
         //ACTION: figure out necessary picture size and reset
         ,
         newCanvas("judgment", 500, 200) //images need to be shuffled, or no?
